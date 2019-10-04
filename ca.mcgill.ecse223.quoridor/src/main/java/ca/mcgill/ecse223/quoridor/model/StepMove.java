2006664aadaf47ecd3625d73636ae3d4b65730fe
pengnan fan <pengnan.fan@mail.mcgill.ca>
2019-10-04 13:52:20 -0400

    create a model

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/model/StepMove.java
@@ -0,0 +1,32 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.0.4181.a593105a9 modeling language!*/
+
+package ca.mcgill.ecse223.quoridor.model;
+
+// line 81 "../../../../../QuoridorGame.ump"
+public class StepMove extends Move
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public StepMove(int aMoveNumber, int aRoundNumber, Player aPlayer, Tile aTargetTile, Game aGame)
+  {
+    super(aMoveNumber, aRoundNumber, aPlayer, aTargetTile, aGame);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
