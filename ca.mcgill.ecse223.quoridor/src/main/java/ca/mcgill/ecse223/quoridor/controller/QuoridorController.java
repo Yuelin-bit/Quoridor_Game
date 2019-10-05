8b503692a2becb9cfc2a530bf56f167952335343
pengnan fan <pengnan.fan@mail.mcgill.ca>
2019-10-05 14:35:51 -0400

    Test

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -1,8 +1,23 @@
 package ca.mcgill.ecse223.quoridor.controller;
 
 import ca.mcgill.ecse223.quoridor.model.Tile;
+import ca.mcgill.ecse223.quoridor.model.User;
 
 public class QuoridorController {
+	
+	/**
+	 * This method takes two users to initialize a new game for them. It will return a boolean value to 
+	 * indicate if a new game is successfully initialized.
+	 * 
+	 * @author Pengnan Fan
+	 * @param user1 The first player who will join in the game
+	 * @param user2 The second player who will join in the game
+	 * @return A boolean value to indicate if a new game is initialized successfully
+	 * 
+	 */
+	public boolean initializeNewGame(User user1, User user2) {
+		throw new UnsupportedOperationException();
+	}
 	
 	public static void loadGame(int number){ 
 		//TO-DO: Write logic to load game
