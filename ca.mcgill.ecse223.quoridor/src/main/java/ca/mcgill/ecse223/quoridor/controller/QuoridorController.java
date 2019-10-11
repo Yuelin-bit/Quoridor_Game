9d0761a7cb9d626d7c102a94f63d5ed025e6132f
Yuelin-bit <yuelinapp@163.com>
2019-10-10 23:44:32 -0400

    java doc

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -2,15 +2,36 @@ package ca.mcgill.ecse223.quoridor.controller;
 
 public class QuoridorController {
 	
+	
+	
+	/**
+	 * Feature:DropWall
+	 * 
+	 * @author Yuelin Liu
+	 * @param release the wall in hand
+	 * @return void
+	 *
+	 *
+	 */
 	public static void ReleaseWall() 
 	{	
+		//TODO: Write logic
 		throw new UnsupportedOperationException();
 	}
 	
 	
-	
+	/**
+	 * Feature:MoveWall
+	 * 
+	 * @author Yuelin Liu
+	 * @param move the wall with specified direction
+	 * @return void
+	 *
+	 *
+	 */
 	public static void MoveWall(String string)
 	{	
+		//TODO: Write logic
 		throw new UnsupportedOperationException();
 	}
 	
