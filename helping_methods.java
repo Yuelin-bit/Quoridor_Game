309efea2b14a1d5b380cbce96484f768347bf247
Cytosine <pengnan.fan@mail.mcgill.ca>
2019-10-29 16:41:22 -0400

    Delete helping_methods.java

--- helping_methods.java
+++
@@ -1,4 +0,0 @@
-/*
-    This is a document to keep all helping methods, in order to avoid "reinvent the wheel." 
-    All helping methods should be present with a full declaration and a brief description.
-*/
