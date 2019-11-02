828813fd3640e24be2e138989a28f922cc08a461
Yuelin-bit <yuelinapp@163.com>
2019-11-02 14:52:58 -0400

    GUI-board..

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -1,11 +1,21 @@
 package ca.mcgill.ecse223.quoridor;
 
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
+import ca.mcgill.ecse223.quoridor.view.Board;
 
 public class QuoridorApplication {
 
 	private static Quoridor quoridor;
 
+	public static void main(String[] args) {
+		// start UI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Board().setVisible(true);
+            }
+        });
+	}
+	
 	public static Quoridor getQuoridor() {
 		if (quoridor == null) {
 			quoridor = new Quoridor();
---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/Board.java
@@ -0,0 +1,187 @@
+package ca.mcgill.ecse223.quoridor.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.JLabel;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+
+public class Board extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5127583580230421217L;
+	private JPanel contentPane;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Board frame = new Board();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	/**
+	 * Create the frame.
+	 */
+	public Board() {
+		setFocusable(true);
+		
+		//Init
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1000, 800);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		getContentPane().add(contentPane);
+		
+		
+		
+		//Create new panel to control layers
+		JPanel mainLayerPanel = new JPanel();		  
+		contentPane.add(mainLayerPanel, BorderLayout.CENTER);		 
+		mainLayerPanel.setBorder(BorderFactory.createLineBorder(Color.orange, 2));
+		mainLayerPanel.setLayout(null);
+		
+		
+		
+		//add tile(with GroupLayout) to the mainLayerPanel
+		Tile tile = new Tile();
+		tile.setBackground(Color.WHITE);
+		tile.setBounds(0, 0, 1000, 800);
+		tile.setOpaque(false);//透明
+		mainLayerPanel.add(tile);
+		
+		
+		
+		//create four labels to specify the operation and set the layout for tile
+		JLabel lblGrabWallPress = new JLabel("Grab Wall: Press G");
+		JLabel lblRotateWallPress = new JLabel("Rotate Wall: Press R");
+		JLabel lblMoveWallPress = new JLabel("Move Wall: Press W,A,S,D");
+		JLabel lblDropWallPress = new JLabel("Drop Wall: Press T");
+		GroupLayout gl_tile = new GroupLayout(tile);
+		gl_tile.setHorizontalGroup(
+			gl_tile.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_tile.createSequentialGroup()
+					.addContainerGap(690, Short.MAX_VALUE)
+					.addGroup(gl_tile.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblGrabWallPress)
+						.addComponent(lblRotateWallPress)
+						.addComponent(lblMoveWallPress, GroupLayout.DEFAULT_SIZE, 184, Short.MAX_VALUE)
+						.addComponent(lblDropWallPress, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGap(178))
+		);
+		gl_tile.setVerticalGroup(
+			gl_tile.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tile.createSequentialGroup()
+					.addGap(121)
+					.addComponent(lblGrabWallPress)
+					.addGap(32)
+					.addComponent(lblRotateWallPress)
+					.addGap(34)
+					.addComponent(lblMoveWallPress)
+					.addGap(37)
+					.addComponent(lblDropWallPress)
+					.addContainerGap(512, Short.MAX_VALUE))
+		);
+		tile.setLayout(gl_tile);
+		
+		
+		
+		Wall wall = new Wall();
+		//add KeyBoard listener!
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (e.getKeyChar()=='g') {					
+					mainLayerPanel.add(wall);
+					wall.setBackground(Color.GRAY);
+					wall.setBounds(120, 60, 10, 110);
+					wall.setVisible(true);
+					System.out.println("yes");
+				}
+				if (e.getKeyChar()=='w') {
+					int x = wall.getLocation().x;
+					int y = wall.getLocation().y;
+					wall.setLocation(x, y-60);
+				}
+				if (e.getKeyChar()=='a') {
+					int x = wall.getLocation().x;
+					int y = wall.getLocation().y;
+					wall.setLocation(x-60, y);
+				}
+				if (e.getKeyChar()=='s') {
+					int x = wall.getLocation().x;
+					int y = wall.getLocation().y;
+					wall.setLocation(x, y+60);
+				}
+				if (e.getKeyChar()=='d') {
+					int x = wall.getLocation().x;
+					int y = wall.getLocation().y;
+					wall.setLocation(x+60, y);
+				}
+				if (e.getKeyChar()=='r') {
+					int x = wall.getLocation().x;
+					int y = wall.getLocation().y;
+					int old_h = wall.getBounds().height;
+					int old_w = wall.getBounds().width;
+					if (old_h==10) {
+						wall.setBounds(x, y, 10, 110);
+					}
+					else {
+						wall.setBounds(x, y, 110, 10);
+					}
+				}
+				if (e.getKeyChar()=='t') {
+					wall.setBackground(Color.red);
+					mainLayerPanel.remove(wall);
+					wall.setBackground(Color.red);
+					mainLayerPanel.add(wall);
+					/*int x = wall.getLocation().x;
+					int y = wall.getLocation().y;
+					int old_h = wall.getBounds().height;
+					int old_w = wall.getBounds().width;
+					Wall wall2 = new Wall();
+					JPanel wall = new JPanel();
+					wall2.setBackground(Color.red);
+					wall2.setBounds(x, y, old_w, old_h);
+					wall2.setVisible(true);
+					mainLayerPanel.remove(wall);
+					wall.setVisible(false);
+					mainLayerPanel.add(wall2);
+					System.out.println("yes2");*/
+				}
+				
+			}
+		});
+		
+		
+		
+	}
+}
