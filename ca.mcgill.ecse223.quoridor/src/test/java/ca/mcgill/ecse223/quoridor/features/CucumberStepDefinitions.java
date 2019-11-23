824eef67196863711668a3ee69da82cc3a35bc3a
QuintusHe <zirui.he@mail.mcgill.ca>
2019-11-23 12:27:16 -0500

    update IdentifyGameStatus

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
@@ -41,6 +41,7 @@ public class CucumberStepDefinitions {
 	private Long whiteStartTime;
 	private Long whiteEndTime;
 	private String error = "";
+	private String gameResult;
 
 	
 	private Quoridor quoridor;
@@ -1321,43 +1322,72 @@ public class CucumberStepDefinitions {
 		@Given("Player {string} has just completed his move")
 		public void player_has_just_completed_his_move(String string) {
 		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
+			Player player;
+			if (string.equals("white")) {
+				player = QuoridorApplication.getQuoridor().getCurrentGame().getWhitePlayer();
+				QuoridorController.completeMove(player);
+			}else {
+				player = QuoridorApplication.getQuoridor().getCurrentGame().getBlackPlayer();
+				QuoridorController.completeMove(player);
+			}
 		}
 
 		@Given("The new position of {string} is {int}:{int}")
 		public void the_new_position_of_is(String string, Integer int1, Integer int2) {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
+			Player player;
+			Tile newTile = QuoridorApplication.getQuoridor().getBoard().getTile(9*(int1-1)+(int2-1));
+			if (string.equals("white")) {
+				player = QuoridorApplication.getQuoridor().getCurrentGame().getWhitePlayer();
+				PlayerPosition newWhitePosition = new PlayerPosition(player, newTile);	
+				QuoridorApplication.getQuoridor().getCurrentGame().getCurrentPosition().setWhitePosition(newWhitePosition);
+			}else {
+				player = QuoridorApplication.getQuoridor().getCurrentGame().getBlackPlayer();
+				PlayerPosition newBlackPosition = new PlayerPosition(player, newTile);	
+				QuoridorApplication.getQuoridor().getCurrentGame().getCurrentPosition().setWhitePosition(newBlackPosition);
+			}
 		}
 
 		@Given("The clock of {string} is more than zero")
 		public void the_clock_of_is_more_than_zero(String string) {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
+			Player player;
+			
+			if (string.equals("white")) {
+				player = QuoridorApplication.getQuoridor().getCurrentGame().getWhitePlayer();
+				
+			} else {
+				player = QuoridorApplication.getQuoridor().getCurrentGame().getBlackPlayer();
+			}
+			
+			Time timeLeft = player.getRemainingTime();
+			int minutes = timeLeft.getMinutes();
+			int seconds = timeLeft.getSeconds();
+			int totaltime = minutes * 60 + seconds;
+			if (totaltime <= 0) {
+				player.setRemainingTime(new Time(3000));
+			}
+			
 		}
 
 		@When("Checking of game result is initated")
 		public void checking_of_game_result_is_initated() {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
+		    gameResult = QuoridorController.checkGameResult();
 		}
 
 		@Then("Game result shall be {string}")
 		public void game_result_shall_be(String string) {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
+			assertEquals(string, gameResult);
 		}
 
 		@Then("The game shall no longer be running")
 		public void the_game_shall_no_longer_be_running() {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
+			GameStatus status = QuoridorApplication.getQuoridor().getCurrentGame().getGameStatus();
+			boolean gameIsRunning = (GameStatus.Running == status);
+			assertEquals(false, gameIsRunning);
 		}
 
 		@When("The clock of {string} counts down to zero")
 		public void the_clock_of_counts_down_to_zero(String string) {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
+			
 		}
 
 
