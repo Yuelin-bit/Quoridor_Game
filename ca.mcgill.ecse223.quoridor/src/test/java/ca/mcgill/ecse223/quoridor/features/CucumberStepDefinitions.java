45da32d38a6403ecb87b0d62ecf6006785f3302b
Gengyi Sun <gengyi.sun@mail.mcgill.ca>
2019-11-23 15:55:22 -0500

    step

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
@@ -1856,11 +1856,79 @@ public class CucumberStepDefinitions {
 		
 		
 		
+		//	*******************************************
+		//	Report Game
+		//	*******************************************
+			
+		@When("The game is no longer running")
+		public void the_game_is_no_longer_running() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Then("The final result shall be displayed")
+		public void the_final_result_shall_be_displayed() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Then("White's clock shall not be counting down")
+		public void white_s_clock_shall_not_be_counting_down() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Then("Black's clock shall not be counting down")
+		public void black_s_clock_shall_not_be_counting_down() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Then("White shall be unable to move")
+		public void white_shall_be_unable_to_move() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Then("Black shall be unable to move")
+		public void black_shall_be_unable_to_move() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+	
+		//	*******************************************
+		//	Check if path exist
+		//	*******************************************
 		
-		
-		
-		
-		
+		@Given("A {string} wall move candidate exists at position {int}:{int}")
+		public void a_wall_move_candidate_exists_at_position(String string, Integer int1, Integer int2) {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Given("The black player is located at {int}:{int}")
+		public void the_black_player_is_located_at(Integer int1, Integer int2) {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Given("The white player is located at {int}:{int}")
+		public void the_white_player_is_located_at(Integer int1, Integer int2) {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@When("Check path existence is initiated")
+		public void check_path_existence_is_initiated() {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
+
+		@Then("Path is available for {string} player\\(s)")
+		public void path_is_available_for_player_s(String string) {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new cucumber.api.PendingException();
+		}
 		
 		
 		
