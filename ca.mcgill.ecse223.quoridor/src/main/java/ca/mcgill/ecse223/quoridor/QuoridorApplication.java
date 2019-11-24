ab86d9e7ab51bcc6d1e436f37c00853bc2fe97ea
Yuelin-bit <yuelinapp@163.com>
2019-11-23 20:43:46 -0500

    music

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -10,7 +10,38 @@ import ca.mcgill.ecse223.quoridor.view.MainMenu;
 import ca.mcgill.ecse223.quoridor.view.NewJBoard;
 import ca.mcgill.ecse223.quoridor.view.NewJBoard4;
 
+import java.applet.AudioClip; 
+import java.io.*; 
+import java.applet.Applet;
+import java.awt.Frame; 
+import java.net.MalformedURLException; 
+import java.net.URI;
+import java.net.URL;
+import javax.swing.JFrame;
+
+
 public class QuoridorApplication {
+
+	@SuppressWarnings("deprecation")
+	static void playMusic(){//背景音乐播放
+		try {
+			URL cb;
+			File f = new File("a"); // 引号里面的是音乐文件所在的路径
+			cb = f.toURL();
+			AudioClip aau;
+			aau = Applet.newAudioClip(cb);
+		
+			aau.play();	
+			aau.loop();//循环播放
+			System.out.println("可以播放");
+			// 循环播放 aau.play()
+			//单曲 aau.stop()停止播放
+ 
+		} catch (MalformedURLException e) {
+			
+			e.printStackTrace();
+		}
+	}
 
 	private static Quoridor quoridor;
 	static MainMenu mainMenu;
@@ -62,6 +93,7 @@ public class QuoridorApplication {
 
 	public static void main(String[] args) {
 		// start UI
+		playMusic();
 		mainMenu = new MainMenu();
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
