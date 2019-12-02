ce1855e380d02c698e230afec08d4f5f333101ea
Gengyi Sun <gengyi.sun@mail.mcgill.ca>
2019-12-01 22:05:03 -0500

    terminate

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/RefreshData.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/RefreshData.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
@@ -18,69 +19,88 @@ public class RefreshData extends Thread{
 	public RefreshData(NewJBoard board) {
 		this.board = board;
 	}
+	@SuppressWarnings("deprecation")
 	public void run() {
 		Player whitePlayer = QuoridorApplication.getQuoridor().getCurrentGame().getWhitePlayer();		
 		Player blackPlayer = QuoridorApplication.getQuoridor().getCurrentGame().getBlackPlayer();		
 		watch = new Stopwatch(whitePlayer,blackPlayer);
 		watch.start();
 		while(true) {
-			GameStatus current = QuoridorApplication.getQuoridor().getCurrentGame().getGameStatus();
 			boolean whiteTurn = QuoridorApplication.getJboard().isWhiteTurn();			
-			boolean blackTurn = !whiteTurn;
-			if(watch.isWhiteTimeIsEnd()&&watch.isBlackTimeIsEnd()){
-				board.getBigTime().setText("Game is END");
-				watch.stop();
-
-			}
-			if(current == GameStatus.BlackWon) {
-				board.getBigTime().setBackground(Color.RED);
-				board.getBigTime().setText("Black Won!");
-			}else if (current == GameStatus.WhiteWon) {
-				board.getBigTime().setBackground(Color.RED);
-				board.getBigTime().setText("White Won!");
-			}
-			if(whiteTurn) {
-				watch.setWhiteRunning(true);
-				watch.setBlackRunning(false);
-				
-				if(watch.isWhiteTimeIsEnd()) {
-					board.getwhiteTime().setText("Time is UP!");
-					QuoridorApplication.getJboard().setWhiteTurn(false);
-
-					
-				}else {
-					String time = QuoridorController.getWhiteRemainingTime();
-					board.getwhiteTime().setText(time);
-					board.getBigTime().setText(time);
+			GameStatus current = QuoridorApplication.getQuoridor().getCurrentGame().getGameStatus();
+			if(	!current.equals(GameStatus.BlackWon)&&
+					!current.equals(GameStatus.WhiteWon)&&
+					!current.equals(GameStatus.Draw)) {
+				if(watch.isWhiteTimeIsEnd()&&watch.isBlackTimeIsEnd()){			
+					watch.stop();
 
 				}
 
-			}else{
-				watch.setWhiteRunning(false);
-				watch.setBlackRunning(true);
-				if(watch.isBlackTimeIsEnd()) {
-					board.getblackTime().setText("Time is UP!");
-					QuoridorApplication.getJboard().setWhiteTurn(true);
+				if(whiteTurn) {
+					watch.setWhiteRunning(true);
+					watch.setBlackRunning(false);
 
-				}else{			
-					String time =QuoridorController.getBlackRemainingTime();
-					board.getblackTime().setText(time);
-					board.getBigTime().setText(time);
+					if(watch.isWhiteTimeIsEnd()) {
+						QuoridorApplication.getQuoridor().getCurrentGame().setGameStatus(GameStatus.BlackWon);
+						board.getBigTime().setBackground(Color.RED);
+						board.getBigTime().setText("Black Won!");
+						board.getwhiteTime().setText("Time is UP!");
+						QuoridorApplication.getJboard().setWhiteTurn(false);
+
+
+					}else {
+						String time = QuoridorController.getWhiteRemainingTime();
+						board.getwhiteTime().setText(time);
+						board.getBigTime().setText(time);
+
+					}
+
+				}else{
+					watch.setWhiteRunning(false);
+					watch.setBlackRunning(true);
+					if(watch.isBlackTimeIsEnd()) {
+						QuoridorApplication.getQuoridor().getCurrentGame().setGameStatus(GameStatus.WhiteWon);
+						board.getBigTime().setBackground(Color.RED);
+						board.getBigTime().setText("White Won!");
+
+						board.getblackTime().setText("Time is UP!");
+						QuoridorApplication.getJboard().setWhiteTurn(true);
+
+					}else{			
+						String time =QuoridorController.getBlackRemainingTime();
+						board.getblackTime().setText(time);
+						board.getBigTime().setText(time);
+					}
+
+
+
 				}
-				
 
-				
-			}
-			
-			try {
-				Thread.sleep(500);
-			}
-			catch(Exception e) {
-				System.out.println("Message:"+e.getMessage());
-				System.out.println("Caused by:"+e.getCause());
+				try {
+					Thread.sleep(500);
+				}
+				catch(Exception e) {
+					System.out.println("Message:"+e.getMessage());
+					System.out.println("Caused by:"+e.getCause());
 
+				}
+			}else {
+				if(current == GameStatus.BlackWon) {
+					board.getBigTime().setForeground(Color.RED);
+					board.getBigTime().setText("Black Won!");
+
+				}else if (current == GameStatus.WhiteWon) {
+					board.getBigTime().setForeground(Color.RED);
+					board.getBigTime().setText("White Won!");
+				}else if(current == GameStatus.Draw) {
+					board.getBigTime().setForeground(Color.RED);
+					board.getBigTime().setText("Draw!");
+				}	
+
+				this.stop();
 			}
+
 		}
 	}
-	
+
 }
